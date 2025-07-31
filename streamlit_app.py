32b99a685511db811f07fd50ab0359dcce2c21b0
miku-taj <115816104+miku-taj@users.noreply.github.com>
2025-08-01 00:38:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import category_encoders as ce
 import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
