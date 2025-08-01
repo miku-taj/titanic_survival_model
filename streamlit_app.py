91587ec04dc99d19bc10d736147042b0a41b086c
miku-taj <115816104+miku-taj@users.noreply.github.com>
2025-08-01 22:13:41 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,11 +143,11 @@ with st.form("user_input_form"):
     embarked_input = st.selectbox("Порт посадки (Embarked)", list(data['Embarked']), index=2)
     prefix_input = st.selectbox("Обращение (Name Prefix)", list(data['Name Prefix'].unique()), index=0)
 
-    age_input = st.number_input("Возраст (Age)", min_value=0, max_value=100, step=1.0)
-    sibsp_input = st.number_input("Братья/сестры или супруг(а) на борту (SibSp)", min_value=int(data['SibSp'].min()), max_value=int(data['SibSp'].max()), step=1)
-    parch_input = st.number_input("Родители/дети на борту (Parch)", min_value=int(data['Parch'].min()), max_value=int(data['Parch'].max()), step=1)
-    fare_input = st.slider("Плата за билет (Fare)", min_value=float(data['Fare'].min()), max_value=float(data['Fare'].max()))
-
+    # age_input = st.number_input("Возраст (Age)", min_value=0, max_value=100, step=1.0)
+    # sibsp_input = st.number_input("Братья/сестры или супруг(а) на борту (SibSp)", min_value=int(data['SibSp'].min()), max_value=int(data['SibSp'].max()), step=1)
+    # parch_input = st.number_input("Родители/дети на борту (Parch)", min_value=int(data['Parch'].min()), max_value=int(data['Parch'].max()), step=1)
+    # fare_input = st.slider("Плата за билет (Fare)", min_value=float(data['Fare'].min()), max_value=float(data['Fare'].max()))
+    
     submitted = st.form_submit_button("Предсказать")
 
 
