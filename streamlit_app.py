d9c770ca8e82b085ec7b127f9d40d663ed65ca9f
miku-taj <115816104+miku-taj@users.noreply.github.com>
2025-08-01 15:10:32 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import category_encoders as ce
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForrestClassifier
+from category_encoders import TargetEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score, roc_auc_score
+
 
 st.set_page_config(page_title="Titanic Survival Model", layout="wide")
 st.title('Titanic Survival Model')
@@ -44,6 +47,7 @@ st.sidebar.markdown('''
 - [Visualization](#visualization)
 ''', unsafe_allow_html=True)
 
+
 st.header("Dataset shape")
 st.write(f"Rows: {data.shape[0]} Columns: {data.shape[1]}")
 
@@ -53,7 +57,6 @@ st.dataframe(data.sample(10), use_container_width=True)
 st.header("Visualization")
 
 sns.set_theme(style="whitegrid", palette="Set2", font_scale=0.6)
-
 fig = plt.figure(figsize=(8, 6))
 gs = fig.add_gridspec(2, 3)
 
@@ -89,19 +92,25 @@ sns.countplot(x='Embarked', hue='Survived', data=data, ax=ax5, alpha=1.0)
 plt.tight_layout()
 st.pyplot(fig, use_container_width=True)
 
-# X = df.drop(["species"], axis=1)
-# y = df["species"]
+X = data.drop('Survived', axis=1)
+y = data['Survived']
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
 
-# encoder = ce.TargetEncoder(cols=['island', 'sex'])
-# X_train_encoded = encoder.fit_transform(X_train, y_train)
-# X_test_encoded = encoder.transform(X_test)
+te = TargetEncoder(cols=['Name Prefix', 'Sex', 'Embarked'])
+X_train_encoded = encoder.fit_transform(X_train, y_train)
+X_test_encoded = encoder.transform(X_test)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 # models = {
 #   'Decision Tree': DecisionTreeClassifier(random_state=42),
 #   'KNN': KNeighborsClassifier()
 # }
+
+model = R
 
 # results = []
 # for name, model in models.items():
