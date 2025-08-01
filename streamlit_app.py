aff56bc1cd67fdc53b6e33142e51b0d00c6114d5
miku-taj <115816104+miku-taj@users.noreply.github.com>
2025-08-01 23:19:58 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,7 +163,7 @@ with st.form("user_input_form"):
 
         user_input_encoded = encoder.transform(user_input)
         for col in ['Age', 'SibSp', 'Parch', 'Fare']:
-        user_input_encoded[col] = user_input[col].values
+            user_input_encoded[col] = user_input[col].values
         user_input_scaled = scaler.transform(user_input_encoded)
         
         with st.expander('See results below'):
